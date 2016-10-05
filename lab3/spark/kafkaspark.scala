5a98fbd8293fde80fb72be01bbf162d75b2c8634
Mateusz Buda <mateusz@antipattern.pl>
2016-10-05 20:14:30 +0200

    added SBT to gitignore and implemented sum by key

--- lab3/spark/kafkaspark.scala
+++ lab3/spark/kafkaspark.scala
@@ -11,30 +11,35 @@ import org.apache.spark.storage.StorageLevel
 object KafkaWordCount {
   def main(args: Array[String]) {
 
-    val kafkaConf = Map(
-	"metadata.broker.list" -> "localhost:9092",
-	"zookeeper.connect" -> "localhost:2181",
-	"group.id" -> "kafka-spark-streaming",
-	"zookeeper.connection.timeout.ms" -> "1000")
+    val kafkaConf = Map[String, String](
+      "metadata.broker.list" -> "localhost:9092",
+      "zookeeper.connect" -> "localhost:2181",
+      "group.id" -> "kafka-spark-streaming",
+      "zookeeper.connection.timeout.ms" -> "1000")
 
-    val sparkConf = new SparkConf().setAppName("KafkaWordCount").setMaster(<FILL IN>)
-    val ssc = new StreamingContext(<FILL IN>)
+    val sparkConf = new SparkConf().setAppName("KafkaWordCount").setMaster("local[2]")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
     ssc.checkpoint("checkpoint")
 
+    var topics = Map[String, Int](
+    "avg" -> 1)
     // if you want to try the receiver-less approach, comment the below line and uncomment the next one
-    val messages = KafkaUtils.createStream[String, String, DefaultDecoder, StringDecoder](<FILL IN>)
+    val messages = KafkaUtils.createStream[String, String, StringDecoder, StringDecoder](ssc, kafkaConf, topics, StorageLevel.MEMORY_ONLY)
     //val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](<FILL IN>)
 
-    val values = messages.map(<FILL IN>)
-    val pairs = values.<FILL IN>
+    val values = messages.map(_._2)
+    val pairs = values.map(x => (x.split(",")(0), x.split(",")(1).toDouble))
 
 
     def mappingFunc(key: String, value: Option[Double], state: State[Double]): Option[(String, Double)] = {
-	<FILL IN>
+      val sum = value.getOrElse(0d) + state.getOption.getOrElse(0d)
+      val output = (key, sum)
+      state.update(sum)
+      Option(output)
     }
 
 
-    val stateDstream = pairs.mapWithState(<FILL IN>)
+    val stateDstream = pairs.mapWithState(StateSpec.function(mappingFunc _))
 
     stateDstream.print()
     ssc.start()
